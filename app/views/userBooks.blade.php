9909ff844b0c5002c0eb2533c04cbc2b72626f7d
Vani Murarka <vani.murarka@gmail.com>
2015-04-30 20:06:23 +0530

    full subtitle in browse and user browse

--- app/views/userBooks.blade.php
+++ app/views/userBooks.blade.php
@@ -66,11 +66,11 @@
 				{{{ $book->Title }}}
 				@if ($book->SubTitle)
 					<div class="bookMatSubTitle">
-					@if (strlen($book->SubTitle)>30)
+					{{--@if (strlen($book->SubTitle)>30)
 						{{{ substr($book->SubTitle,0,30).'...' }}}
-					@else
+					@else--}}
 						{{{ $book->SubTitle }}}
-					@endif
+					{{--@endif--}}
 					</div>
 				@endif
 				</a>
