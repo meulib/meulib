a70c1d636caedb009d02c39f4a664e5366d1fe40
Vani Murarka <vani.murarka@gmail.com>
2015-05-12 08:51:39 +0530

    home page image width per screen

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,7 +10,7 @@
 	}
 	$appName = Config::get('app.name');
 	$whatsNew = Config::get('app.whatsnew');
-	$randomPromo = rand(1,4);
+	$randomPromo = rand(1,5);
 ?>
 
 @section('content')
@@ -54,8 +54,37 @@
 				{{$singleNew}}<br/><br/>
 			@endforeach
 		</p-->
-		<blockquote class="twitter-tweet" data-partner="tweetdeck"><p lang="en" dir="ltr">Looks like <a href="https://twitter.com/hashtag/Piku?src=hash">#Piku</a> is inundated. Are you too?&#10;Give away books you no longer want on <a href="http://t.co/KEDcFfBxth">http://t.co/KEDcFfBxth</a> <a href="http://t.co/KKRskLAuai">pic.twitter.com/KKRskLAuai</a></p>&mdash; Meulib (@meulib) <a href="https://twitter.com/meulib/status/595796037199110144">May 6, 2015</a></blockquote>
-<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+		<!-- blockquote class="twitter-tweet" data-partner="tweetdeck"><p lang="en" dir="ltr">Looks like <a href="https://twitter.com/hashtag/Piku?src=hash">#Piku</a> is inundated. Are you too?&#10;Give away books you no longer want on <a href="http://t.co/KEDcFfBxth">http://t.co/KEDcFfBxth</a> <a href="http://t.co/KKRskLAuai">pic.twitter.com/KKRskLAuai</a></p>&mdash; Meulib (@meulib) <a href="https://twitter.com/meulib/status/595796037199110144">May 6, 2015</a></blockquote>
+		<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script -->
+		<div style="max-width:100%;text-align:center">
+		@if ($randomPromo == 1)
+			<a href={{ URL::to('/how-it-works-owner') }}>
+			{{ HTML::image('images/howitworks/o1.png','',array('width'=>287, 'height'=>246,
+						'style'=>'max-width:100%')) }}
+			<br/>
+			See how {{Config::get('app.name')}} works<br/>
+			for the book owner</a>
+		@endif
+		@if ($randomPromo == 2)
+			{{HTML::image('images/promo/vision.png','',array('style'=>'max-width:100%'))}}
+		@endif
+		@if ($randomPromo == 3)
+			<a href={{ URL::to('/how-it-works-borrower') }}>
+			{{ HTML::image('images/howitworks/b1.png','',array('width'=>287, 'height'=>246,
+					'style'=>'max-width:100%')) }}
+			<br/>
+			See how {{Config::get('app.name')}} works<br/>
+			for the borrower</a>
+		@endif
+		@if ($randomPromo == 4)
+			{{ HTML::image('images/promo/directLending.png','',array('style'=>'max-width:100%')) }}
+		@endif
+		@if ($randomPromo == 5)
+			Looks like Piku is inundated. Are you too?<br/>
+			Give away books you no longer want.<br/>
+			{{ HTML::image('images/promo/pikuFB.jpg','',array('width'=>600,'style'=>'max-width:100%')) }}
+		@endif
+		</div>
 	</div>
 	
 	</div>
