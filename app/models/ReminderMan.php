4f81b8676f2d2ea57af055b725e8e3f619c8092e
Vani Murarka <vani.murarka@gmail.com>
2015-09-13 13:18:21 +0530

    blog-link

---
+++ app/models/ReminderMan.php
@@ -0,0 +1,20 @@
+<?php
+
+class ReminderMan {
+
+	public static function checkIfReminderToBeSent()
+	{
+		
+	}
+
+	private static function SendReturnReminderOwner()
+	{
+	}
+
+	private static function SendReturnReminderLender()
+	{
+	}
+
+}
+
+?>
